8481aa6ad55764804fb4121a23889da17793493f
rohit-persistent <rohit_kumar1@persistent.co.in>
2021-02-12 17:48:24 +0530

    changed occupied beds path and data

--- actions/actions.py
+++ actions/actions.py
@@ -23,7 +23,7 @@ import logging
 logger = logging.getLogger(__name__)
 
 root_dir= "/app/data/covid19-resource-allocation-ui/data_va_durations/"
-beds_data_path="/app/data/covid19-resource-allocation-ui/data_va/"
+beds_data_path="/app/data/covid19-resource-allocation-ui/data_va_durations/"
 
 log_file_location="/app/logs/"
 
