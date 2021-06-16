072f71a6f50a2266d66b0dff741f59931455f160
rushikeshphalke137 <rushikesh_phalke@persistent.com>
2021-06-16 18:24:36 +0530

    Update chatbot_config.py

--- chatbot_config.py
+++ chatbot_config.py
@@ -1,4 +1,4 @@
 prod = {"chatbot_title":"US",
         "is_actual_exists":False,
-        "data_path":"data/covid19-resource-allocation-ui/data_us_actuals"
+        "data_path":"/app/data/covid19-resource-allocation-ui/data_us_actuals"
        }    
