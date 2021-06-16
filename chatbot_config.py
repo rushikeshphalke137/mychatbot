037579a30323edc19e7716be308ace375ad52416
rushikeshphalke137 <rushikesh_phalke@persistent.com>
2021-06-16 19:10:53 +0530

    Update chatbot_config.py

--- chatbot_config.py
+++ chatbot_config.py
@@ -1,4 +1,4 @@
-prod = {"chatbot_title":"Virginia",
+prod = {"chatbot_title":"US",
         "is_actual_exists":True,
-        "data_path":"data/covid19-resource-allocation-ui/data_va_actuals"
+        "data_path":"data/covid19-resource-allocation-ui/data_us_actuals"
        }    
